220aadbea4ef6b7e7567f21795558fc936e1dc8a
Garrett Thompson <gtcruz.gt@gmail.com>
2020-10-11 01:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 // Write your solution in this file!
-const driver = {};
+var recipes = new Object ({});
 
-function updateDriverWithKeyAndValue(obj, key, value) {
-  newDriver = Object.assign({}, obj);
-  newDriver[key] = value
-
-  return newDriver;
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, obj, {[key]: value})
 };
-
-function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
-  obj[key] = value;
-
-  return obj;
-}
-
-function deleteFromDriverByKey(obj, key) {
-  deleteDriver = Object.assign({}, obj)
-  delete deleteDriver[key]
-
-  return deleteDriver;
-}
-
-function destructivelyDeleteFromDriverByKey(obj, key) {
-  delete obj[key];
-
-  return obj;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+};
+var newObj = Object.assign({}, obj);
+function deleteFromObjectByKey(object, key){
+  delete newObj[key];
+  return newObj
+};
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
